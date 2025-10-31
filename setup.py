0063625d6f7875f52673918a61fa3793e3361529
Anton Pasternak <antpaster@gmail.com>
2025-10-31 07:46:13 +0300

    Add: script for building cpp extension.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from torch.utils.cpp_extension import CppExtension, BuildExtension
+
+setup(
+    name="fused_add_relu_ext",
+    ext_modules=[
+        CppExtension(
+            "fused_add_relu_ext",
+            ["fused_add_relu.cpp"],  # put C++ code in a file
+            extra_compile_args=["-O3","-march=native","-mavx2","-std=c++17"],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
